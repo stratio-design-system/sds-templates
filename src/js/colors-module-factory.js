524177b6882cb056bf61c9bfdcfe19bdc8cb0bed
Nul Pàs <manu@lunaeme.com>
2021-05-06 07:29:39 +0200

    SDS :: Templates v.5.1.0  · Aligned project to Medea.  · Updated dependencies.

--- src/js/colors-module-factory.js
+++ src/js/colors-module-factory.js
@@ -23,7 +23,7 @@ const colors = {};
 let group = '';
 let colorIdsForInterface = '';
 let colorIdsForObject = '';
-const groupInterface = 'Array<SdsColor>';
+const groupInterface = 'Array<NpaColor>';
 let groupNames = '';
 
 lineReader.on('line', (line) => {
@@ -58,11 +58,11 @@ lineReader.on('line', (line) => {
 
 lineReader.on('close', () => {
   const file = fs.createWriteStream(path.resolve(__dirname, 'ts', 'colors.ts'));
-  file.write('export interface SdsColor { name: string; label: string; color: string; rgb: number[]; cmyk: number[]; yiq: number; }');
-  file.write(`export interface SdsColorScheme { ${groupNames} }`);
-  file.write(`export interface SdsColorNames { ${colorIdsForInterface} }`);
-  file.write(`export const sdsColorNames: SdsColorNames = { ${colorIdsForObject} };`);
-  file.write('export const sdsColors: SdsColorScheme = ' + JSON.stringify(colors) + ';');
+  file.write('export interface NpaColor { name: string; label: string; color: string; rgb: number[]; cmyk: number[]; yiq: number; }');
+  file.write(`export interface NpaColorScheme { ${groupNames} }`);
+  file.write(`export interface NpaColorNames { ${colorIdsForInterface} }`);
+  file.write(`export const npaColorNames: NpaColorNames = { ${colorIdsForObject} };`);
+  file.write('export const npaColors: NpaColorScheme = ' + JSON.stringify(colors) + ';');
   file.end();
 
   console.log('Build done. (Module Colors)');
--- src/js/ts/index.ts
+++ src/js/ts/index.ts
@@ -1,26 +1,26 @@
-import { SdsColor, sdsColorNames, SdsColorNames, sdsColors, SdsColorScheme } from './colors';
+import { NpaColor, npaColorNames, NpaColorNames, npaColors, NpaColorScheme } from './colors';
 
-export class SdsTemplate {
-  public colors: SdsColorScheme;
+export class NpaTemplate {
+  public colors: NpaColorScheme;
 
-  private readonly _colorFlatArray: Array<SdsColor> = [];
-  private readonly _colorNames: SdsColorNames;
+  private readonly _colorFlatArray: Array<NpaColor> = [];
+  private readonly _colorNames: NpaColorNames;
   private readonly _colorNamesArray: Array<string>;
 
   constructor() {
-    this.colors = sdsColors;
+    this.colors = npaColors;
 
     for (const group of Object.keys(this.colors)) {
       this._colorFlatArray = [...this._colorFlatArray, ...this.colors[group]];
     }
-    this._colorNames = sdsColorNames;
+    this._colorNames = npaColorNames;
     this._colorNamesArray = Object.keys(this._colorNames);
   }
 
-  public getColorByName(colorName: string): SdsColor {
+  public getColorByName(colorName: string): NpaColor {
     return this._colorNamesArray.includes(colorName) ?
-      this._colorFlatArray.find((e: SdsColor) => e.name === colorName) : null;
+      this._colorFlatArray.find((e: NpaColor) => e.name === colorName) : null;
   }
 }
 
-export const sdsTemplate = new SdsTemplate();
+export const npaTemplate = new NpaTemplate();
